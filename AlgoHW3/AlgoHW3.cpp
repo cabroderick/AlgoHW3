a02166632e924fdd54bdc2232648018723e06802
Collin Broderick <cabroderick@wpi.edu>
2019-11-18 16:07:04 -0500

    Finished question 3 altogether; question 2 unfinished still

--- AlgoHW3/AlgoHW3.cpp
+++ AlgoHW3/AlgoHW3.cpp
@@ -1,36 +1,71 @@
 #include <string>
 #include <iostream>
+#include <vector>
+using namespace std;
+
 
 // Generats recursively the binary reflected Gray code of order n
 // Input: A positive integer n
 // Output: A list of all bit strings of length n composing the Gray code
-std::string* BRGC(int n) {
+std::vector<std::string> BRGC(int n) {
 	if (n == 1) { // If the length is 0, the only possible bit strings are 0 and 1
-		std::string L[2] = { "0", "1" };
-		return &L[0];
-	}
-	else {
-		std::string* L1 = BRGC(n - 1);
-		int size = pow(2, n); // The size of arrays L1 and L2
-		std::string* L2 = (std::string*) malloc(sizeof(std::string*) * size);
-		for (int i = 0; i < size; i++) {
-			L2[size - i - 1] = L1[i];
-			L1[i].append("0");
-			L2[size - i - 1].append("1");
-		}
-		std::string* L = (std::string*) malloc(sizeof(std::string*) * (size * 2));
-		for (int i = 0; i < size * 2; i++) { // Appends both arrays into new array
-			if (i < size) { // Array L1
-				L[i] = L1[i];
-			}
-			else { // Array L2
-				L[i] = L2[i - size];
-			}
-		}
-		free(L1);
-		free(L2);
+		vector<string> L = { "0", "1" };
 		return L;
 	}
+	else {
+		vector<string> L1 = BRGC(n - 1);
+		vector<string> L2 (L1.rbegin(), L1.rend());
+		for (int i = 0; i < L1.size(); i++) {
+			L1.at(i).insert(0, "0");
+			L2.at(i).insert(0, "1");
+		}
+		vector<string> L;
+		L.insert(L.end(), L1.begin(), L1.end());
+		L.insert(L.end(), L2.begin(), L2.end());
+		return L;
+	}
+}
+
+void greyCodesAreFun() {
+	vector<string> L = BRGC(4);
+	for (int i = 1; i < L.size(); i++) {
+		bool alice = false;
+		bool bob = false;
+		bool chris = false;
+		bool dylan = false;
+		if (L.at(i)[3] == '1') alice = true;
+		if (L.at(i)[2] == '1') bob = true;
+		if (L.at(i)[1] == '1') chris = true;
+		if (L.at(i)[0] == '1') dylan = true;
+		string name = ""; // The name of the person moving
+		string action = ""; // In/Out
+		int a = stoi(L.at(i), nullptr, 2); // Gets base 2 version of binary representation
+		int b = stoi(L.at(i - 1), nullptr, 2);
+		int diff = a - b; // Difference between successive list elements
+		if (diff > 0) action = "In";
+		else action = "Out";
+		switch (abs(diff)) {
+		case(1):
+			name = "Alice";
+			break;
+		case(2):
+			name = "Bob";
+			break;
+		case(4):
+			name = "Chris";
+			break;
+		case(8):
+			name = "Dylan";
+			break;
+		}
+		cout << "Index: " << i << " Gray code: " << L.at(i) << " Children: ";
+		if (alice) cout << "Alice ";
+		if (bob) cout << "Bob ";
+		if (chris) cout << "Chris ";
+		if (dylan) cout << "Dylan ";
+		cout << "Action: " << name << " " << action << "\n";
+	}
+
 }
 
 // Counts the number of inversions in an array of doubles
@@ -50,24 +85,24 @@ int easyInversionCount(double nums[], int size) {
 // l: The left index for the first
 // m: The middle index for the first
 // r: The right index for the second
-int merge(double* Arr, int start, int mid, int end) {
+int merge(double* Arr, int left, int middle, int right) {
 
 	int sum = 0;
 	// create a temp array
-	double* temp = (double*)malloc(sizeof(double) * (end - start + 1));
+	double* temp = (double*)malloc(sizeof(double) * (right - left + 1));
 
 	// crawlers for both intervals and for temp
-	int i = start, j = mid + 1, k = 0;
+	int i = left, j = middle + 1, k = 0;
 
 	// traverse both arrays and in each iteration add smaller of both elements in temp 
-	while (i <= mid && j <= end) {
+	while (i <= middle && j <= right) {
 		if (Arr[i] <= Arr[j]) {
 			temp[k] = Arr[i];
 			k += 1; i += 1;
 		}
 		else {
 			temp[k] = Arr[j];
-			sum = sum + (mid - i); // Index sum counter
+			sum = sum + (middle - i); // Index sum counter
 			// NEW LINE RIGHT HERE
 			k += 1; j += 1;
 			
@@ -75,20 +110,20 @@ int merge(double* Arr, int start, int mid, int end) {
 	}
 
 	// add elements left in the first interval
-	while (i <= mid) {
+	while (i <= middle) {
 		temp[k] = Arr[i];
 		k += 1; i += 1;
 	}
 
 	// add elements left in the second interval 
-	while (j <= end) {
+	while (j <= right) {
 		temp[k] = Arr[j];
 		k += 1; j += 1;
 	}
 
 	// copy temp to original interval
-	for (i = start; i <= end; i += 1) {
-		Arr[i] = temp[i - start];
+	for (i = left; i <= right; i += 1) {
+		Arr[i] = temp[i - left];
 	}
 	return sum;
 }
@@ -99,7 +134,7 @@ int merge(double* Arr, int start, int mid, int end) {
 // right: The index of the right of the array
 int fastInversionCount(double* nums, int left, int right) {
 	int count = 0;
-	if (left < right) {
+	if (right > left) {
 		int middle = (left + right) / 2;
 		count += fastInversionCount(nums, left, middle);
 		count += fastInversionCount(nums, middle + 1, right);
@@ -146,7 +181,12 @@ int main() {
 	double nums3[7] = { 38, 27, 43, 3, 9, 82, 10 };
 	std::cout << easyInversionCount(nums2, 5) << "\n";
 	std::cout << fastInversionCount(nums2, 0, 4) << "\n";
-	//std::string* arr = BRGC(5);
+	std::vector<std::string> arr = BRGC(4);
+	for (int i = 0; i < arr.size(); i++) {
+		cout << arr.at(i) << " ";
+	}
+	cout << "\n";
+	greyCodesAreFun();
 }
 
 // First, write function that reverses string
